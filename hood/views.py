f9cee146a575547a032376540e5dcb122f191ce5
dan-jugz <njugunadaniel364@gmail.com>
2020-01-20 13:31:53 +0300

    define a function that allows a user to create a hood in the webpage

--- hood/views.py
+++ hood/views.py
@@ -36,3 +36,15 @@ def hoods(request):
     }
     return render(request, 'all_hoods.html', params)
 
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
