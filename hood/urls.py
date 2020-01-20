6c4bbddffaf2b89d5c2304b7e752be1074ed11b6
dan-jugz <njugunadaniel364@gmail.com>
2020-01-20 13:40:47 +0300

    set a route for all hoods and new hoods templates

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
 ]
